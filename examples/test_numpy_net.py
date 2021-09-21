16402cb4ac7b64fa6c23d8355eee2da76e0e7503
javier <lopez.randulfe@tum.de>
2021-09-21 13:48:33 +0200

    Add relative error plots to the example

--- examples/test_numpy_net.py
+++ examples/test_numpy_net.py
@@ -13,9 +13,9 @@ def main(conf_filename="../config/test_experiment.json"):
     # Instantiate a simulation handler and run spiking FT with sample data
     sim_handler = spikingFT.startup.startup(conf_filename)
     nsamples = sim_handler.snn.nsamples
-    real_spikes = sim_handler.output[:, 0]
-    imag_spikes = sim_handler.output[:, 1]
-    sft_modulus = np.sqrt(real_spikes**2 + imag_spikes**2)[1:int(nsamples/2)]
+    real_spikes = sim_handler.output[:, 0][1:int(nsamples/2)]
+    imag_spikes = sim_handler.output[:, 1][1:int(nsamples/2)]
+    sft_modulus = np.sqrt(real_spikes**2 + imag_spikes**2)
 
     # Get reference FT result from NumPy
     ft_np = np.fft.fft(sim_handler.data[0, 0, :, 0])
@@ -41,12 +41,38 @@ def main(conf_filename="../config/test_experiment.json"):
     rel_error = sim_handler.metrics["rel_error"]
     real_error = rel_error[:, 0]
     imag_error = rel_error[:, 1]
-    plt.subplot(2, 1, 1)
-    plt.plot(real_error)
-    plt.title("Real spectrum rel. error")
-    plt.subplot(2, 1, 2)
-    plt.plot(imag_error)
-    plt.title("Imaginary spectrum rel. error")
+    # Real spectrum
+    ax_left = plt.subplot(3, 1, 1)
+    ax_right = ax_left.twinx()
+    l1 = ax_left.plot(real_spikes, color="blue", label="signal")
+    l2 = ax_right.plot(real_error, color="red", linestyle="--", label="error")
+    ax_right.set_ylim([0., 0.05])
+    ax_left.set_ylabel("|FT(x)|")
+    ax_right.set_ylabel("Rel. error")
+    lines = l1 + l2
+    labels = [l.get_label() for l in lines]
+    ax_left.legend(lines, labels)
+    ax_left.set_title("Real spectrum rel. error")
+    #Left spectrum
+    ax_left = plt.subplot(3, 1, 2)
+    ax_right = ax_left.twinx()
+    l1 = ax_left.plot(imag_spikes, color="blue", label="signal")
+    l2 = ax_right.plot(imag_error, color="red", linestyle="--", label="error")
+    ax_right.set_ylim([0., 0.05])
+    ax_left.set_ylabel("|FT(x)|")
+    ax_right.set_ylabel("Rel. error")
+    ax_left.set_title("Imaginary spectrum rel. error")
+    # Modulus
+    abs_error = (real_error + imag_error) / 2
+    ft_modulus = np.sqrt(real_spikes**2 + imag_spikes**2)
+    ax_left = plt.subplot(3, 1, 3)
+    ax_right = ax_left.twinx()
+    l1 = ax_left.plot(ft_modulus, color="blue", label="signal")
+    l2 = ax_right.plot(abs_error, color="red", linestyle="--", label="error")
+    ax_right.set_ylim([0., 0.05])
+    ax_left.set_ylabel("|FT(x)|")
+    ax_right.set_ylabel("Rel. error")
+    ax_left.set_title("Imaginary spectrum rel. error")
 
     plt.tight_layout()
     plt.show()
